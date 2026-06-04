ffd3b6832e02eb24b7892f70beb565b7e990a7d5
StrelnicekarlinCZ <strelnicekarlin@gmail.com>
2026-06-04 12:02:59 +0200

    Update route.ts

--- app/api/reminders/route.ts
+++ app/api/reminders/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
+import QRCode from 'qrcode';
 import { dbEnabled, prisma } from '../../../lib/prisma';
 
 export const runtime = 'nodejs';
